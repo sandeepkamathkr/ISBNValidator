c7b2f255d37726e16cc732d3b2c2fb0075d1f423
Sandeep <sandeepkamathkr@gmail.com>
2020-07-17 17:22:47 +1000

    ISBN Validator TDD to check both 13 and 10 digit ISBN number

--- src/com/sandeepkamathkr/isbntools/ValidateISBN.java
+++ src/com/sandeepkamathkr/isbntools/ValidateISBN.java
@@ -2,12 +2,23 @@ package com.sandeepkamathkr.isbntools;
 
 public class ValidateISBN {
 
+    public static final int LONG_ISBN_LENGTH = 13;
+    public static final int SHORT_ISBN_LENGTH = 10;
+    public static final int SHORT_ISBN_MULTIPLIER = 11;
+    public static final int LONG_ISBN_MULTIPLIER = 10;
+
     public boolean checkISBN(String isbnNumber) {
-        if (isbnNumber.length() != 10) {
-            throw new NumberFormatException("ISBN number must be 10 digit long");
+        if (isbnNumber.length() == LONG_ISBN_LENGTH) {
+            return isThisAValidLongISBN(isbnNumber);
+        } else if (isbnNumber.length() == SHORT_ISBN_LENGTH){
+            return isThisAValidShortISBN(isbnNumber);
         }
+        throw new NumberFormatException("ISBN number must be 10 or 13 digit long");
+    }
+
+    private boolean isThisAValidShortISBN(String isbnNumber) {
         int total = 0;
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < SHORT_ISBN_LENGTH; i++) {
             if (!Character.isDigit(isbnNumber.charAt(i))) {
                 if (i == 9 && isbnNumber.charAt(i) == 'X') {
                     total += 10;
@@ -15,14 +26,22 @@ public class ValidateISBN {
                     throw new NumberFormatException("ISBN numbers " +
                             "can only contain numeric digits");
                 }
-            }else {
-                total += Character.getNumericValue(isbnNumber.charAt(i)) * (10 - i);
+            } else {
+                total += Character.getNumericValue(isbnNumber.charAt(i)) * (SHORT_ISBN_LENGTH - i);
             }
         }
-        if (total % 11 == 0) {
-            return true;
-        } else {
-            return false;
+        return total % SHORT_ISBN_MULTIPLIER == 0;
+    }
+
+    private boolean isThisAValidLongISBN(String isbnNumber) {
+        int total = 0;
+        for (int i = 0; i < LONG_ISBN_LENGTH; i++) {
+            if (i % 2 == 0) {
+                total += Character.getNumericValue(isbnNumber.charAt(i));
+            } else {
+                total += Character.getNumericValue(isbnNumber.charAt(i)) * 3;
+            }
         }
+        return total % LONG_ISBN_MULTIPLIER == 0;
     }
 }
--- test/com/sandeepkamathkr/isbntools/ExampleTests.java
+++ test/com/sandeepkamathkr/isbntools/ExampleTests.java
@@ -16,16 +16,32 @@ public class ExampleTests {
     }
 
     @Test
-    public void ISBNNumbersEndingInAnXAreValid(){
+    public void tenDigitISBNNumbersEndingInAnXAreValid(){
         ValidateISBN validator = new ValidateISBN();
         boolean result = validator.checkISBN("012000030X");
         assertTrue(result);
     }
     @Test
-    public void checkAnInValidISBN(){
+    public void checkAnInValid10DigitISBN(){
         ValidateISBN validator = new ValidateISBN();
         boolean result = validator.checkISBN("0140449117");
         assertFalse(result);
+    }
+
+    @Test
+    public void checkAnInValid13DigitISBN(){
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("9781853267336");
+        assertFalse(result);
+    }
+
+    @Test
+    public void checkAValid13DigitISBN(){
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("9781853260087");
+        assertTrue("first value",result);
+        result = validator.checkISBN("9781853267338");
+        assertTrue("second value",result);
     }
 
     @Test(expected = NumberFormatException.class)
